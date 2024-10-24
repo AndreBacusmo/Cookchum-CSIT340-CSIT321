8dded7c20ba72cf772fc57e19426f6e7a2f37599
Ikanstie <uniskowt@gmail.com>
2024-10-24 12:31:52 +0800

    Added CookChum Project

---
+++ cookchum/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './home';
+import './App.css'; // Import styles for App
+
+const App = () => {
+  return (
+    <div className="App">
+      <Home /> {/* Render the Home component */}
+    </div>
+  );
+};
+
+export default App;
